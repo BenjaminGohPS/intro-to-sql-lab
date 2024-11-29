3933904970a769b23a6a02e37607666a857f128d
benjamingohps <benjamingohps@gmail.com>
2024-11-29 17:22:10 +0800

    clue 7 solved. Carmen Sandiego, here we come.

--- clues.sql
+++ clues.sql
@@ -74,3 +74,7 @@ WHERE countries.name in (
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT name AS city FROM cities
+WHERE population = 91084;
+-- ANSWER: Santa Monica -> Because she is going to a city to increase the population by 1 to 91,085
